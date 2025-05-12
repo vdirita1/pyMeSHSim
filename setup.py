27172ed6fd453008566301d9b98b0e8adecf6cf7
Victor Di Rita <victordirita@mac.mynetworksettings.com>
2025-05-12 17:02:00 -0400

    Update setup.py to allow modern NumPy and pandas versions

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
-#!/usr/bin/env python3
 from setuptools import setup, find_packages
+import io
+import os
+
+# Read the long description from README.md
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name="pyMeSHSim",
+    name='pyMeSHSim',
     version='0.0.1',
-    description="tools for MeSH system operation",
-    author="luozhihui",
-    author_email='luozhihui1123@163.com',
-    # url="https://github.com/username/reponame",
-    # download_url='https://github.com/username/reponame/archive/0.1.tar.gz',
-    keywords=['MeSH', 'Metamap', 'similarity'],
+    description='MeSH semantic similarity computations using UMLS and ontology metrics',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Victor Di Rita',
+    author_email='vdirita1@example.com',
+    url='https://github.com/vdirita1/pyMeSHSim',
     packages=find_packages(),
-    include_package_data=True,
-
+    python_requires='>=3.7',
+    install_requires=[
+        'bcolz>=1.2.1',
+        'numpy>=1.20.0',
+        'pandas>=1.3.0',
+        'Cython>=0.29.0'
+    ],
     classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Operating System :: POSIX',
-        'Development Status :: 1 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Topic :: Scientific/Engineering :: Bio-Meidecine'
-        ],
-    install_requires=['bcolz>=1.2.1',
-                        'pandas>=1.3.0']
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    include_package_data=True,
 )
